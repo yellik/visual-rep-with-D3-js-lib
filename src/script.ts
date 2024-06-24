c598b36de41e4736f2c28b73cc89bc84f7c2bf7d
Yellik <yelenakensborn@gmail.com>
2024-06-24 12:16:16 +0200

    added data

---
+++ src/script.ts
@@ -0,0 +1,12 @@
+var data = [
+    {country: "Nigeria", usage: 45},
+    {country: "Thailand", usage: 44},
+    {country: "Turkey", usage: 40},
+    {country: "Argentina", usage: 35},
+    {country: "UAE", usage: 34},
+    {country: "Philippines", usage: 29},
+    {country: "Vietname", usage: 27},
+    {country: "Singapore", usage: 25},
+    {country: "South Africa", usage: 23},
+    {country: "Brazil", usage: 23},
+]
